63c2601e0681462c24241030d6729be8abe509f3
Sean Hoops <50555459+SeanathonHooper@users.noreply.github.com>
2025-06-15 22:54:05 -0400

    Added brick behavior

---
+++ Patches/BrickBehavior.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx.Logging;
+using UnityEngine;
+namespace ThrowableBrick.Patches
+{
+    class BrickBehavior : PhysicsProp
+    {
+
+        private Ray brickThrowRay;
+        private int brickMask = 268437761;
+        public RaycastHit brickHit;
+        bool hasCollided = false;
+        private int health = 3;
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+
+            playerHeldBy.DiscardHeldObject(true, null, GetThrowDestination());
+        }
+
+        public override void OnHitGround()
+        {
+            base.OnHitGround();
+            health--;
+            if (health <= 0)
+            {
+                Landmine.SpawnExplosion(transform.position, true);
+                DestroyObjectInHand(playerHeldBy);
+            }
+        }
+
+        public override void EquipItem()
+        {
+            base.EquipItem();
+            hasCollided = false;
+        }
+        public Vector3 GetThrowDestination()
+        {
+            Vector3 position = base.transform.position;
+            Debug.DrawRay(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward, Color.yellow, 15f);
+            brickThrowRay = new Ray(playerHeldBy.gameplayCamera.transform.position, playerHeldBy.gameplayCamera.transform.forward);
+            position = ((!Physics.Raycast(brickThrowRay, out brickHit, 12f, brickMask, QueryTriggerInteraction.Ignore)) ? brickThrowRay.GetPoint(10f) : brickThrowRay.GetPoint(brickHit.distance - 0.05f));
+            Debug.DrawRay(position, Vector3.down, Color.blue, 15f);
+            brickThrowRay = new Ray(position, Vector3.down);
+            if (Physics.Raycast(brickThrowRay, out brickHit, 30f, brickMask, QueryTriggerInteraction.Ignore))
+            {
+                return brickHit.point + Vector3.up * 0.05f;
+            }
+            return brickThrowRay.GetPoint(30f);
+        }
+    }
+
+}
--- ThrowableBrick.cs
+++ ThrowableBrick.cs
@@ -3,6 +3,11 @@ using BepInEx.Logging;
 using HarmonyLib;
 using LobbyCompatibility.Attributes;
 using LobbyCompatibility.Enums;
+using UnityEngine;
+using LethalLib;
+using System.IO;
+using System.Reflection;
+using ThrowableBrick.Patches;
 
 namespace ThrowableBrick;
 
@@ -15,6 +20,8 @@ public class ThrowableBrick : BaseUnityPlugin
     internal new static ManualLogSource Logger { get; private set; } = null!;
     internal static Harmony? Harmony { get; set; }
 
+    public static AssetBundle BrickAsset;
+
     private void Awake()
     {
         Logger = base.Logger;
@@ -23,6 +30,9 @@ public class ThrowableBrick : BaseUnityPlugin
         Patch();
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        Logger.LogInfo("FUCK!");
+
+        InitializeItem();
     }
 
     internal static void Patch()
@@ -43,5 +53,27 @@ public class ThrowableBrick : BaseUnityPlugin
         Harmony?.UnpatchSelf();
 
         Logger.LogDebug("Finished unpatching!");
+    }
+
+    internal static void InitializeItem()
+    {
+        string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        BrickAsset = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "brickassetbundle"));
+        if (BrickAsset == null)
+        {
+            Logger.LogInfo("Failed to load custom assets");
+            return;
+        }
+
+        int rarity = 100000;
+        Item throwableBrickItem = BrickAsset.LoadAsset<Item>("Assets/BRICK/BrickItem.asset");
+        BrickBehavior brickBehavior = throwableBrickItem.spawnPrefab.AddComponent<BrickBehavior>();
+        brickBehavior.grabbable = true;
+        brickBehavior.grabbableToEnemies = true;
+        brickBehavior.itemProperties = throwableBrickItem;
+        throwableBrickItem.minValue = 40;
+        throwableBrickItem.maxValue = 80;
+        LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(throwableBrickItem.spawnPrefab);
+        LethalLib.Modules.Items.RegisterScrap(throwableBrickItem, rarity, LethalLib.Modules.Levels.LevelTypes.All, null);
     }
 }
